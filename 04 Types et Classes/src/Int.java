220f97b9ec0e801d0c424e342440dabf03b322db
Sacha Bron <sacha.bron@copperflake.com>
2014-10-09 13:28:24 +0200

    Int created. Ex 2 done.

--- 04 Types et Classes/src/Int.java
+++ 04 Types et Classes/src/Int.java
@@ -1,4 +1,24 @@
 
 public class Int {
+	private int value;
 
+	public Int() {
+		
+	}
+	
+	public Int(int val) {
+		setValue(value);
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	public String toString() {
+		return String.valueOf(getValue());
+	}
 }
