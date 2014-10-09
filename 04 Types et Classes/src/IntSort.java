61a57fe906735bbdb7aa432795293b03fc449bf9
D34D10K <leonard.berney@epfl.ch>
2014-10-09 14:17:51 +0200

    sort1

--- 04 Types et Classes/src/IntSort.java
+++ 04 Types et Classes/src/IntSort.java
@@ -21,4 +21,23 @@ public class IntSort {
         }
         System.out.println(Arrays.toString(tab));
     }
+    public Int[] static swap(Int[] tab, int el1, int el2) {
+        Int tmp =  tab[el1];
+        tab[el1] = tab[el2];
+        tab[el2] = tmp;
+
+        return tab;
+    }
+
+    public static Int[] sort1(Int[] tab) {
+        for (int i = 0; i < tab.length ; i++ ) {
+            for (int j = 1; j < tab.length - i; j++ ) {
+                if (tab[j - 1] > tab[j]) {
+                    tab = swap(tab, j, j - 1);
+                }
+            }
+        }
+
+        return tab;
+    }
 }
