42fc85bf7076ccc48a7ebdd9b707371a180de959
Sacha Bron <sacha.bron@copperflake.com>
2014-10-16 14:04:34 +0200

    v1.0

--- 05 Matrices/src/Launcher.java
+++ 05 Matrices/src/Launcher.java
@@ -10,7 +10,12 @@ public class Launcher {
 		
 		System.out.println("Matrix one\n" + m1);
 		System.out.println("Matrix two\n" + m2);
-
+		System.out.println("oneOrTwo = one or two");
+		System.out.println(new Or().executeOperation(m1, m2));
+		System.out.println("oneAndTwo = one and two");
+		System.out.println(new And().executeOperation(m1, m2));
+		System.out.println("oneXOrTwo = one xor two");
+		System.out.println(new Xor().executeOperation(m1, m2));
 	}
 
 }
