0b187ac91b50bf1883da0f47db5d0f8f78accebd
ValentinMinder <valentin.minder@epfl.ch>
2014-10-16 13:57:19 +0200

    main 2 matrix creation

--- 05 Matrices/src/Launcher.java
+++ 05 Matrices/src/Launcher.java
@@ -2,8 +2,14 @@
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello");
+		System.out.println("Welcome to the Matrix Generator!");
+		
+		int size = 4;
+		Matrix m1 = new Matrix(size);
+		Matrix m2 = new Matrix(size);
+		
+		System.out.println("Matrix one\n" + m1);
+		System.out.println("Matrix two\n" + m2);
 
 	}
 
