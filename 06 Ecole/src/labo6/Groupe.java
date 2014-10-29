74281870f24812fa6f2aa18736924a896c98b397
Sacha Bron <sacha.bron@copperflake.com>
2014-10-29 13:22:13 +0100

    skeleton of Groupe.java and Lecon.java

---
+++ 06 Ecole/src/labo6/Groupe.java
@@ -0,0 +1,33 @@
+/*
+ * File: Groupe.java
+ * Authors: Simon Baehler, Sacha Bron
+ * Date: 29.10.2014
+ * Goal: ???
+ */
+
+package labo6;
+
+public class Groupe {
+	private int numero;
+	private int orientation;
+	private int trimestre;
+	
+	public String horaire() {
+		return null;
+		
+	}
+	
+	public String nom() {
+		return null;
+		
+	}
+	
+	public int nombreEtudiants() {
+		return -1;
+		
+	}
+	
+	public void definirLecon(/* args */) {
+		
+	}
+}
---
+++ 06 Ecole/src/labo6/Lecon.java
@@ -0,0 +1,21 @@
+/*
+ * File: Lecon.java
+ * Authors: Simon Baehler, Sacha Bron
+ * Date: 29.10.2014
+ * Goal: ???
+ */
+
+
+package labo6;
+
+public class Lecon {
+	private String matiere;
+	private int jourSemaine;
+	private int periodeDebut;
+	private int duree;
+	private String salle;
+	
+	public String horaire(/* args */) {
+		return null;
+	}
+}
